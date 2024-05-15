74b740d6adfad2c4ca20983f306171a6395eaf3a
Nazar <prokopevnazar4@gmail.com>
2024-05-15 19:05:55 +0300

    Update JS project

---
+++ index.js
@@ -0,0 +1,63 @@
+console.log('hi!');
+
+// створіть змінну яка буде зберігати час коли ви прокидаєтесь
+
+let wakeUpTime = '10:30';
+console.log(wakeUpTime);
+
+// створіть змінну яка буде зберігати кількість днів у тиждні
+
+const daysPerWeek = 7;
+console.log("Days per week:", daysPerWeek);
+
+//створіть змінну, яка містить булеве значення чи є ви працівником
+
+const isWork = false;
+
+
+// створіть змінну, яка зберігає ваш зріст в метрах 1.84
+
+const myHeight = 1.84;
+console.log:(myHeight);
+
+// чи є машина?
+
+const isCar = false;
+
+const titleMovie = 'Titanic';
+console.log(titleMovie);
+titleMovie = 'Titanic2';
+
+// створіть змінну для вашої домашньої тварини і запишіть що зе за тварина я або null
+
+let pet = null;
+
+// ім'я
+
+let firstName = 'Nazar';
+console.log(firstName);
+
+
+
+let isAdult = true;
+let isWeekend = false;
+
+// створіть змінну яка буде містити булеве значення про те чи є ви студентом
+let isStudent = false;
+
+let yearOfBirth = 1900;
+console.log(yearOfBirth);
+let yearBD = 1978;
+console.log(yearOfBirth);
+
+//створіть змінну для зберігання вашого улюленного кольору
+
+let my_favoriteColor = 'purple';
+console.log= (my_favoriteColor);
+
+let age = null;
+console.log(age);
+age=45;
+console.log(age);
+
+
